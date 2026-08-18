9e7ff01152164aec5a525670b2e811269a746611
Eliezir <eliezirneto@gmail.com>
2026-08-18 20:20:18 -0300

    feat(rota-02): responde GET /sobre com HTML e título Sobre

--- index.js
+++ index.js
@@ -22,10 +22,15 @@ if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Olá, Mundo!');
   }
+else if (req.method === 'GET' && req.url === '/sobre') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Sobre</h1>');
+  }
 else {
   res.writeHead(404, { 'Content-Type': 'text/plain' });
   res.end('Não encontrado');
 }
+
 
 });
 
