e9ae75e024b2b19eddcdd8642cb76c58ecc825c8
Eliezir <eliezirneto@gmail.com>
2026-08-18 20:25:33 -0300

    feat(rota-03): responde GET /saudacao/:nome personalizada

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ else if (req.method === 'GET' && req.url === '/sobre') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Sobre</h1>');
   }
+else if (req.method === 'GET' && req.url.startsWith('/saudacao/')) {
+    const nome = req.url.split('/')[2];
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Olá, ${nome}!`);
+  }
 else {
   res.writeHead(404, { 'Content-Type': 'text/plain' });
   res.end('Não encontrado');
